06ed144f00c9a82c7b7888af94cfb96820e48004
amirsa025 <amirsaki06@gmail.com>
2022-07-13 17:45:11 +0430

    fix bug search

--- src/component/Search/Search.js
+++ src/component/Search/Search.js
@@ -6,31 +6,35 @@ import {
     TextInput,
     View
 } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {useNote} from "../../context/NoteProvider";
 import keyboard from "react-native-web/dist/exports/Keyboard";
 
 const Search = () => {
     const [searchNote, SetSearchNote] = React.useState('')
-    const {setData} = useNote()
+    const {setData ,Data ,findNotes} = useNote()
     const handleSearchQuery = async (SearchNote) => {
         SetSearchNote(SearchNote)
         const result = await AsyncStorage.getItem('note_Key')
         let note = []
         if (result !== null) note = JSON.parse(result)
+        const SearchFilterNote = note.filter((item) => {
+            if (searchNote==='') {
 
-        const filterNote = note.filter((item)=>{
-            if(searchNote===""){
                 return item
             }
-            if(item.text.toLowerCase().includes(searchNote.toLowerCase())){
-                    return  item
+            if (item.text.toLowerCase().includes(searchNote.toLowerCase())) {
+
+                return item
             }
         })
-        setData([...filterNote])
+        if(SearchFilterNote.length){
+            setData([...SearchFilterNote])
+        }
     }
-    const handleClear = async() => {
-        SetSearchNote('')
+    const handleClear = async () => {
+        SetSearchNote(' ')
         const result = await AsyncStorage.getItem('note_Key');
         if (result !== null) setData(JSON.parse(result));
     }
@@ -43,6 +47,7 @@ const Search = () => {
                        autoCorrect={false}
                        clearButtonMode="always" defaultValue={searchNote} placeholder='SearchNote...'
                        style={styles.input} onChangeText={handleSearchQuery}/>
+            <AntDesign name="close" size={16} color="black" onPress={handleClear}  />
 
         </View>
     );
@@ -52,15 +57,17 @@ export default Search;
 
 const styles = StyleSheet.create({
     container: {
-        width: "80%",
+        width :"100%",
+        flexDirection:"row",
+        alignItems : "center"
     },
 
     input: {
+        width:"80%",
         marginLeft: 20,
         height: 50,
         flexDirection: "row",
         alignItems: "center",
-        outline: 0,
         paddingLeft: 5
     }
 })
--- src/context/NoteProvider.js
+++ src/context/NoteProvider.js
@@ -7,10 +7,16 @@ const noteContext = createContext();
 const NoteProvider = ({children}) =>{
     const [Data, setData] = React.useState([])
 
-
+    const findNotes = async () => {
+        const result = await AsyncStorage.getItem('notes');
+        if (result !== null) setData(JSON.parse(result));
+    };
+    useEffect(async () => {
+     await   findNotes();
+    }, []);
 
     return(
-        <noteContext.Provider value={{Data , setData}}>
+        <noteContext.Provider value={{Data , setData ,findNotes}}>
             {children}
         </noteContext.Provider>
     )
